68cf280c5b2608ac4b9810d585d8593eb83c0d22
v54087912-collab <v54087912@gmail.com>
2025-12-06 21:35:27 +0530

    Bot UPDATE 20251206_213507

---
+++ Notes-3-app-fix-vercel-deploy/public/sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'waveplay-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/js/songs.js',
+    '/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log('Caching assets');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+    // Strategy: Cache First, then Network
+    // For MP3s, we might want to cache them as they are played
+
+    if (event.request.url.endsWith('.mp3')) {
+        event.respondWith(
+            caches.open('waveplay-music-cache').then((cache) => {
+                return cache.match(event.request).then((response) => {
+                    return response || fetch(event.request).then((networkResponse) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    });
+                });
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
